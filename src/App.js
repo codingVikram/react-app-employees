baa5fe4d98bca244924294f76c1580eec33e3197
codngVikram <multani_v@hotmail.com>
2024-02-03 15:22:26 +0000

    added modal and styled form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import "./index.css";
 import Employee from "./components/Employee";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [role, setRole] = useState("dev"); // hooks
+
+  const [employees, setEmployees] = useState([
+    {
+      name: "Caleb",
+      role: "Developer",
+      img: "https://images.pexels.com/photos/1261427/pexels-photo-1261427.jpeg",
+    },
+    {
+      name: "Sal",
+      role: "Manager",
+      img: "https://images.pexels.com/photos/5717512/pexels-photo-5717512.jpeg",
+    },
+    {
+      name: "John",
+      role: "Director of Eng.",
+      img: "https://images.pexels.com/photos/3760514/pexels-photo-3760514.jpeg",
+    },
+    {
+      name: "Maelanie",
+      role: "Software Engineer",
+      img: "https://images.pexels.com/photos/4050291/pexels-photo-4050291.jpeg",
+    },
+    {
+      name: "Corey",
+      role: "The Devops guy",
+      img: "https://images.pexels.com/photos/3345882/pexels-photo-3345882.jpeg",
+    },
+    {
+      name: "Jake",
+      role: "Senior Intern",
+      img: "https://images.pexels.com/photos/5483063/pexels-photo-5483063.jpeg",
+    },
+  ]);
 
   const showEmployees = true;
   return (
@@ -17,9 +51,18 @@ function App() {
               setRole(e.target.value);
             }}
           />
-          <Employee name={"Vikram"} role="Intern" />
-          <Employee name={"Abby"} role={role} />
-          <Employee name={"John"} />
+          <div className="flex flex-wrap justify-center">
+            {employees.map((employee) => {
+              return (
+                <Employee
+                  key={uuidv4}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                />
+              );
+            })}
+          </div>
         </>
       ) : (
         <p>You cannot see the employees</p>
