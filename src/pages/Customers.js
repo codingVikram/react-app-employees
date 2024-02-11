b24673eeb43aa69143225e76682535d5edb49ad9
codingVikram <multani_v@hotmail.com>
2024-02-11 10:22:12 +0000

    request customers from custom backend

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,3 +1,29 @@
-export default function () {
-  return <h1>Hello there!</h1>;
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Customers() {
+  const [customers, setCustomers] = useState();
+  useEffect(() => {
+    console.log("Fetching...");
+    fetch("http://localhost:8000/api/customers/")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setCustomers(data.customers);
+      });
+  }, []);
+  return (
+    <>
+      <h1>Here are our customers:</h1>
+      {customers
+        ? customers.map((customer) => {
+            return (
+              <p>
+                <Link to={"/customers/" + customer.id}>{customer.name}</Link>
+              </p>
+            );
+          })
+        : null}
+    </>
+  );
 }
